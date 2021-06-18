233ddc3fd7e6ec3e9856578b91a44f78162fbb10
JP Villalobos <jp.villalobos@philips.com>
2021-06-17 21:14:31 -0600

    cosmetics

--- example-smart-app/lib/js/patient-interaction.js
+++ example-smart-app/lib/js/patient-interaction.js
@@ -14,6 +14,7 @@ function getAuthorization() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var json = JSON.parse(xhr.responseText);
             global_auth_object = json;
+            document.getElementById("IDButton").disabled = false;
             console.log(json);
         }
     };
@@ -176,40 +177,3 @@ function UserAction() {
     function getCOData(){
         getPatientData();
     }
-
-// Builds the HTML Table out of myList json data from Ivy restful service.
-function buildHtmlTable(arr) {
-    var table = _table_.cloneNode(false),
-      columns = addAllColumnHeaders(arr, table);
-    for (var i = 0, maxi = arr.length; i < maxi; ++i) {
-      var tr = _tr_.cloneNode(false);
-      for (var j = 0, maxj = columns.length; j < maxj; ++j) {
-        var td = _td_.cloneNode(false);
-        cellValue = arr[i][columns[j]];
-        td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
-        tr.appendChild(td);
-      }
-      table.appendChild(tr);
-    }
-    return table;
-  }
-  
-  // Adds a header row to the table and returns the set of columns.
-  // Need to do union of keys from all records as some records may not contain
-  // all records
-  function addAllColumnHeaders(arr, table) {
-    var columnSet = [],
-      tr = _tr_.cloneNode(false);
-    for (var i = 0, l = arr.length; i < l; i++) {
-      for (var key in arr[i]) {
-        if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1) {
-          columnSet.push(key);
-          var th = _th_.cloneNode(false);
-          th.appendChild(document.createTextNode(key));
-          tr.appendChild(th);
-        }
-      }
-    }
-    table.appendChild(tr);
-    return columnSet;
-  }
